12368a487fcd3d5acc5fc70cb91f72f7bd594e44
Alexandru Savin <alexandrusavin@gmail.com>
2017-02-20 16:07:18 +0100

    Handle large multiplier efficiently

--- PasteRepeater.py
+++ PasteRepeater.py
@@ -2,9 +2,17 @@ import sublime, sublime_plugin
 
 class RepeaterCommand(sublime_plugin.TextCommand):
   def run(self, edit):
-    self.view.window().show_input_panel("Paste how many times?", "", self.on_done, None, None)
+    clip = sublime.get_clipboard();
+    self.view.window().show_input_panel("(%s)Paste how many times?" % clip, "", self.on_done, None, None)
 
   def on_done(self, user_input):
     pasteCount = int(float(user_input))
+    clip = sublime.get_clipboard();
+    text = '';
+
     for x in range(0,pasteCount):
-      self.view.run_command('paste')
+        text += clip
+
+    sublime.set_clipboard(text);
+    self.view.run_command('paste');
+    sublime.set_clipboard(clip);
