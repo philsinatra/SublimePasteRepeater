4f1928a84b2e8aff3b2428dbb3592c53d210fbd3
Phil Sinatra <code@philsinatra.com>
2017-03-06 08:19:26 -0500

    Remove Preview Text
    Preview text in paste dialog box removed because of small screen
    display issues.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,4 +3,13 @@ All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [0.1.0] - 2015-12-21
+
 - initial release
+
+## [1.0.0] - 2017-02-22
+
+- Change large multiplier handler. (@alexandrusavin fix)
+
+## [1.0.1] - 2017-03-06
+
+- Remove preview text in dialog box. (@Kristinita fix)
--- PasteRepeater.py
+++ PasteRepeater.py
@@ -3,7 +3,8 @@ import sublime, sublime_plugin
 class RepeaterCommand(sublime_plugin.TextCommand):
   def run(self, edit):
     clip = sublime.get_clipboard();
-    self.view.window().show_input_panel("(%s)Paste how many times?" % clip, "", self.on_done, None, None)
+    # self.view.window().show_input_panel("(%s)Paste how many times?" % clip, "", self.on_done, None, None)
+    self.view.window().show_input_panel("PasteRepeater: How many times? ", "", self.on_done, None, None)
 
   def on_done(self, user_input):
     pasteCount = int(float(user_input))
